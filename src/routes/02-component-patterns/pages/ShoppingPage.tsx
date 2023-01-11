2f36eb6cdbb8a130a4f6fbf3933e4b85ec9f9f03
falfaro <falfaro@aligare.cl>
2023-01-10 23:51:46 -0300

    pre-npom listo

--- src/routes/02-component-patterns/pages/ShoppingPage.tsx
+++ src/routes/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
 import { products } from '../data/data';
-import '../styles/custom-styles.css';
 
 const product = products[0]
 
@@ -14,7 +13,6 @@ export const ShoppingPage = () => {
             <ProductCard
                 key={product.id}
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 4,
                     maxCount: 10
@@ -22,13 +20,9 @@ export const ShoppingPage = () => {
             >
                 {({ reset, increaseBy, isMaxCountReached }) => (
                     <>
-                        <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                        <ProductTitle className="text-bold" />
-                        <ProductButtons className="custom-buttons" />
-
-                        <button onClick={reset}>Reset</button>
-                        <button onClick={() => increaseBy(-2)}>-2</button>
-                        <button onClick={() => increaseBy(2)} hidden={isMaxCountReached}>+2</button>
+                        <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
+                        <ProductTitle />
+                        <ProductButtons />
                     </>
                 )}
 
